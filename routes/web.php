0e03970cedee0700eae1a5222087548ce4187a50
WonSeok Seo <koak5089@naver.com>
2021-01-17 03:12:29 +0900

    method route error

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('products','ProductContrlloer@index')->name('products.index');
+Route::get('products','ProductContrlloer@create');
+
+
 
 Route::get('/', function () {
     return view('welcome');
